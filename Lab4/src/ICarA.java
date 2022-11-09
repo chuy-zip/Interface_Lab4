ecdef347b269d4b172d767fac5123de24bc2b8c6
chuy-zip <74995966+chuy-zip@users.noreply.github.com>
2022-11-09 17:31:54 -0600

    Added necessary methods to the  Car A and B

--- Lab4/src/ICarA.java
+++ Lab4/src/ICarA.java
@@ -1,6 +1,27 @@
 
 public interface ICarA {
+	/**
+	 * Metodo para obtener si la llamada esta en altavos o en modo auricular
+	 * true = esta en speaker
+	 * @return
+	 */
+	public boolean getSpeakerStatus();
 	
-		
+	/**
+	 * Metodo para cambiar el estado de llamada entre speaker y auriculares
+	 * la variable se cambia en el metodo y en el objeto
+	 * @param LastCalledContact
+	 */
+	public boolean ChangeSpeakerStatus();
 	
+	/**
+	 * Metodo para regresar la planificacion de un viaje
+	 * @param tripday dia del viaje
+	 * @param tripHour_ hora de comeinzo del viaje
+	 * @param tripPlace A donde sera el viaje
+	 * @param tripLength Cuanto durara el viaje completo
+	 * @param tripObjective descripcion y motivo del viaje
+	 * @return Cadena modificada que describa la planificacion del viaje
+	 */
+	public String TripPlanification(String tripday, String tripHour_, String tripPlace, String tripLength, String tripObjective);
 }
--- Lab4/src/ICarB.java
+++ Lab4/src/ICarB.java
@@ -1,4 +1,29 @@
-
+/**
+ * Interface para los carros tipo B, estos deben de poder hacer lo que hace el C
+ * mas las funcionalidades que implemente esta interfaz
+ * @author andre
+ *
+ */
 public interface ICarB {
-
+	
+	/**
+	 * Metodo para obtener el ultimo contacto al que se llamo
+	 * @return
+	 */
+	public String getLastContact();
+	
+	/**
+	 * Metodo para cambiar el ultimo contacto guardado
+	 * @param LastCalledContact
+	 */
+	public void setLastContact(String LastCalledContact);
+	
+	/**
+	 * Metodo para obtener las tarjetas de presentacion
+	 * Queda libre como se guardaran estas, ya que estaran pre establecidas por 
+	 * cada grupo en el codigo 
+	 * @return
+	 */
+	public String getPresentationCards();
+	
 }
